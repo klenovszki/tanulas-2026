c14997544f6b9cc4526496e991d0d785f85129d1
Klenovszki János <janos.klenovszki@nrc.hu>
2026-05-09 05:29:02 +0200

    Nap 4 - dotenv használat env-test.js scripttel

---
+++ nap2-elso-script/env-test.js
@@ -0,0 +1,8 @@
+// Nap 4 - .env titkok beolvasása
+require('dotenv').config();
+
+console.log("=================================");
+console.log("API kulcs teszt");
+console.log("=================================");
+console.log("Az API kulcsom:", process.env.API_KEY);
+console.log("=================================");
